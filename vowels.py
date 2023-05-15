ccad86ef72afe2970ccbcd87517916f3f226d9cb
anindita-s <110987573+anindita-s@users.noreply.github.com>
2023-05-15 18:23:33 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,1 +1,7 @@
-
+def count_vowels(string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for char in string:
+        if char.lower() in vowels:
+            count += 1
+    return count
